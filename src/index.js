07385f0d488e3936ef41b7c22cab207c3f0eece6
Atit Timilsina <atitpraloud@gmail.com>
2020-07-23 13:12:57 +0545

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar backgroundColor={Colors.primaryColor} />
+        <Text>
+          Corona App!
+       </Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default App;
