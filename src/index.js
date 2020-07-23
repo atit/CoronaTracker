cb7feb694f07fa2e3043e5e22688bcdc3cdf2e53
Atit Timilsina <atitpraloud@gmail.com>
2020-07-23 14:17:12 +0545

    Fetch data at Home page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 
 import { Colors } from './constants';
+import { fetchData } from '../api';
 
 
 const styles = StyleSheet.create({
@@ -15,6 +16,14 @@ const styles = StyleSheet.create({
   },
 });
 class App extends Component {
+  state = {
+    data: {},
+    country: '',
+  }
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
   render() {
     return (
       <SafeAreaView style={styles.container}>
